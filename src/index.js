1c60a43b6e0aaf283c98394bc04727a67503af19
juliomchado <juliocarlos00@hotmail.com>
2021-12-14 20:22:21 +0000

    add/ all http methods + reqs

--- src/index.js
+++ src/index.js
@@ -2,12 +2,63 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({
-        message: "Hello World"
-    })
+app.use(express.json());
 
-})
+app.get("/courses", (req, res) => {
+    const query = req.query;
+
+    console.log(query);
+
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+    ]);
+});
+
+app.post("/courses", (req, res) => {
+
+    const body = req.body;
+
+    console.log(body)
+
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4",
+    ]);
+});
+
+app.put("/courses/:id", (req, res) => {
+    const params = req.params
+
+    console.log(params)
+
+    return res.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4",
+    ]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 6",
+        "Curso 7",
+        "Curso 3",
+        "Curso 4",
+    ]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 4",
+    ]);
+});
 
 app.listen(3333, () => {
     console.log('Application running in 3333')
